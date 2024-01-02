4ceb0299304cc3b931def1eb24cc65ed542e87dd
KAHRAMAN KAYA <khrmn8375@gmail.com>
2024-01-02 16:53:55 +0300

    Add files via upload

---
+++ Project12/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args){
+        for(int i=5; i>0; i--){
+
+            int j=0;
+            while(j<i){
+                System.out.print(j);
+                j++;
+
+            }
+
+        System.out.println();
+        }
+
+
+    }
+}
