f112eb96191ffd7add33ccc73c6ec3e512fdfdcd
kahraman kaya <khrmn8375@mail.com>
2024-01-03 13:05:28 +0300

    .

---
+++ Project12/src/App2.java
@@ -0,0 +1,9 @@
+public class App2 {
+    public static void main(String[] args) {
+        for(int i=0; i<5;i++){
+            for(j=i; j>1;j++){
+                
+            }
+        }
+    }
+}
