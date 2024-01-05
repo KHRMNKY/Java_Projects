d226b435a9fdbed3be652bdf6d9682c0ad3f5bee
kahraman kaya <khrmn8375@mail.com>
2024-01-05 12:12:01 +0300

    App.java

---
+++ Project14/App.java/src/App.java
@@ -0,0 +1,24 @@
+/*
+ * "Konsolda alt alta aşağıdaki iki farklı mesajı toplamda on bin adet çıktı veren Java programını yazınız."
+ */
+
+public class App{
+    public static void main(String[] args) {
+        int i=0;
+        int m=0;
+        while(m<10000){
+            System.out.println("Yazilim mühendisliği " + i);
+            i++;
+            System.out.println("cokk calismaliyiz    "+ i);
+            i++;
+            m++;
+             
+        } 
+
+    }
+}
+
+
+
+
+
