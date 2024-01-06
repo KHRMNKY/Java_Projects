cdb6ac5d946bb4e15d10622f266caad857c3f1ba
kahraman kaya <khrmn8375@mail.com>
2024-01-06 13:12:02 +0300

    Project16

---
+++ Project16/src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum;
+        int number1;
+        int number2;
+
+        do {
+            number1 = (int) (Math.random() * 10);
+            number2 = (int) (Math.random() * 10);
+            System.out.println(number1 + " ile " + number2 + " toplamını giriniz.");
+            sum = sc.nextInt();
+            System.out.println("Yanlış topladınız, tekrar deneyiniz.");
+        } 
+        while (sum != (number1 + number2));
+            System.out.println("Tebrikler, doğru cevabı girdiniz.");
+        
+
+            
+        
+
+    }
+}
+
